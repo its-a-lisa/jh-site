9e6f21b83e772f1e3c57ffbe10d5d627f1e883f7
Alyssa Feola <65581330+its-a-lisa@users.noreply.github.com>
2025-02-18 00:07:25 +0000

    activated dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,32 @@
+import ThemeContext from '@/components/plasmic/17_sds_foundation_global/PlasmicGlobalVariant__Theme';
 import '@/styles/globals.css'
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
+import { DataProvider } from '@plasmicapp/react-web/lib/host';
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Link from "next/link";
+import { useState } from 'react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
+
   return (
+    <DataProvider 
+    name='darkModeData'
+    data={{
+      darkMode,
+      toggleDarkMode
+    }}
+    >
+      <ThemeContext.Provider value={darkMode ? "dark" : undefined}>
     <PlasmicRootProvider Head={Head} Link={Link}>
       <Component {...pageProps} />
     </PlasmicRootProvider>
+    </ThemeContext.Provider>
+    </DataProvider>
   );
 }
